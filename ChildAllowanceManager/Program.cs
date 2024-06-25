fa4bcc34b30cde095724323cff29d83f7ffc9138
Aieat Assam <AieatAssam@users.noreply.github.com>
2024-06-26 00:49:24 +0100

    Fixed azure monitor configuration. Added EasyAuth endpoint hyperlinks for testing.

--- ChildAllowanceManager/Program.cs
+++ ChildAllowanceManager/Program.cs
@@ -1,4 +1,7 @@
 using System.Globalization;
+using System.Net;
+using System.Security.Claims;
+using System.Security.Principal;
 using Azure.Monitor.OpenTelemetry.AspNetCore;
 using ChildAllowanceManager.Common.Interfaces;
 using ChildAllowanceManager.Common.Models;
@@ -6,6 +9,7 @@ using ChildAllowanceManager.Components;
 using ChildAllowanceManager.Services;
 using ChildAllowanceManager.Workers;
 using MudBlazor.Services;
+using Newtonsoft.Json.Linq;
 using Quartz;
 
 // Edit culture to match the desired one
@@ -73,5 +77,61 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+// from https://learn.microsoft.com/en-us/answers/questions/1373805/how-to-get-user-name-for-blazor-server-application
+app.Use(async (context, next) =>
+{
+    // Create a user on current thread from provided header
+    if (context.Request.Headers.ContainsKey("X-MS-CLIENT-PRINCIPAL-ID"))
+    {
+        // Read headers from Azure
+        var azureAppServicePrincipalIdHeader = context.Request.Headers["X-MS-CLIENT-PRINCIPAL-ID"][0];
+        var azureAppServicePrincipalNameHeader = context.Request.Headers["X-MS-CLIENT-PRINCIPAL-NAME"][0];
+
+        #region extract claims via call /.auth/me
+
+        //invoke /.auth/me
+        var cookieContainer = new CookieContainer();
+        HttpClientHandler handler = new HttpClientHandler()
+        {
+            CookieContainer = cookieContainer
+        };
+        string uriString = $"{context.Request.Scheme}://{context.Request.Host}";
+        foreach (var c in context.Request.Cookies)
+        {
+            cookieContainer.Add(new Uri(uriString), new Cookie(c.Key, c.Value));
+        }
+
+        string jsonResult = string.Empty;
+        using (HttpClient client = new HttpClient(handler))
+        {
+            var res = await client.GetAsync($"{uriString}/.auth/me");
+            jsonResult = await res.Content.ReadAsStringAsync();
+        }
+
+        //parse json
+        var obj = JArray.Parse(jsonResult);
+        string user_id = obj[0]["user_id"].Value<string>(); //user_id
+
+        // Create claims id
+        List<Claim> claims = new List<Claim>();
+        foreach (var claim in obj[0]["user_claims"])
+        {
+            claims.Add(new Claim(claim["typ"].ToString(), claim["val"].ToString()));
+        }
+
+        // Set user in current context as claims principal
+        var identity = new GenericIdentity(azureAppServicePrincipalNameHeader);
+        identity.AddClaims(claims);
+
+        #endregion
+
+        // Set current thread user to identity
+        context.User = new GenericPrincipal(identity, null);
+    }
+
+    await next.Invoke();
+});
+
 
 app.Run();
