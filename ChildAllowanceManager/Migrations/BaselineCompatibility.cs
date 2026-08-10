219cc3f279a2ed6d50246eeb4351542ae2e5e60b
Aieat Assam <t_l_m_p@yahoo.co.uk>
2026-08-10 22:52:00 +0100

    fix(migrations): add an EnsureCreated compatibility baseline

---
+++ ChildAllowanceManager/Migrations/BaselineCompatibility.cs
@@ -0,0 +1,34 @@
+using ChildAllowanceManager.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChildAllowanceManager.Migrations;
+
+public static class BaselineCompatibility
+{
+    public static async Task EnsureBaselineRecordedAsync(AllowanceDbContext db, CancellationToken ct)
+    {
+        var historyExists = await db.Database
+            .SqlQueryRaw<bool>("SELECT to_regclass('public.\"__EFMigrationsHistory\"') IS NOT NULL AS \"Value\"")
+            .SingleAsync(ct);
+        if (historyExists)
+            return;
+
+        var tenantsExist = await db.Database
+            .SqlQueryRaw<bool>("SELECT to_regclass('public.\"Tenants\"') IS NOT NULL AS \"Value\"")
+            .SingleAsync(ct);
+        if (!tenantsExist)
+            return;
+
+        await db.Database.ExecuteSqlRawAsync(
+            """
+            CREATE TABLE "__EFMigrationsHistory" (
+                "MigrationId" character varying(150) NOT NULL,
+                "ProductVersion" character varying(32) NOT NULL,
+                CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY ("MigrationId"));
+            """, ct);
+
+        await db.Database.ExecuteSqlRawAsync(
+            "INSERT INTO \"__EFMigrationsHistory\" (\"MigrationId\", \"ProductVersion\") VALUES ({0}, {1});",
+            [db.Database.GetMigrations().First(), db.Model.GetProductVersion()], ct);
+    }
+}
