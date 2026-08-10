6431c22968b16d359a6dca69eb0e4ab47998b5d9
Aieat Assam <t_l_m_p@yahoo.co.uk>
2026-08-10 22:47:13 +0100

    fix(migrations): scaffold a real baseline migration and model snapshot

---
+++ ChildAllowanceManager/Migrations/20260810214537_Initial.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ChildAllowanceManager.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Children",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    FirstName = table.Column<string>(type: "text", nullable: false),
+                    LastName = table.Column<string>(type: "text", nullable: false),
+                    BirthDate = table.Column<DateTime>(type: "date", nullable: true),
+                    RegularAllowance = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    HoldDaysRemaining = table.Column<int>(type: "integer", nullable: false),
+                    BirthdayAllowance = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: true),
+                    TenantId = table.Column<string>(type: "text", nullable: false),
+                    Deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedTimestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedTimestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Children", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tenants",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    TenantName = table.Column<string>(type: "text", nullable: false),
+                    UrlSuffix = table.Column<string>(type: "text", nullable: false),
+                    Deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedTimestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedTimestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tenants", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transactions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Balance = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    TransactionAmount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    ChildId = table.Column<string>(type: "text", nullable: false),
+                    TenantId = table.Column<string>(type: "text", nullable: false),
+                    TransactionTimestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    TransactionType = table.Column<int>(type: "integer", nullable: false),
+                    AllowanceDate = table.Column<DateTime>(type: "date", nullable: true),
+                    Deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedTimestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedTimestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transactions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Roles = table.Column<string[]>(type: "text[]", nullable: false),
+                    Tenants = table.Column<string[]>(type: "text[]", nullable: false),
+                    LastLoggedIn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    Deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    CreatedTimestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedTimestamp = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Children_TenantId_Deleted",
+                table: "Children",
+                columns: new[] { "TenantId", "Deleted" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tenants_UrlSuffix",
+                table: "Tenants",
+                column: "UrlSuffix",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_TenantId_ChildId_AllowanceDate",
+                table: "Transactions",
+                columns: new[] { "TenantId", "ChildId", "AllowanceDate" },
+                unique: true,
+                filter: "\"AllowanceDate\" IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transactions_TenantId_ChildId_TransactionTimestamp",
+                table: "Transactions",
+                columns: new[] { "TenantId", "ChildId", "TransactionTimestamp" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Children");
+
+            migrationBuilder.DropTable(
+                name: "Tenants");
+
+            migrationBuilder.DropTable(
+                name: "Transactions");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
